a6c4a68615ac99b7efe934cb27a0efdb98c2e7fa
Mike Izbicki <mike@izbicki.me>
2019-09-24 09:11:35 -0700

    Update lab.py

--- lab_03/lab.py
+++ lab_03/lab.py
@@ -41,6 +41,71 @@ def is_prime(n):
         False
     '''
 
+def fizz_buzz(n):
+    '''
+    for each number between 1 and n:
+        if the number is a multiple of 3, print 'Fizz',
+        if the number is a multiple of 5, print 'Buzz',
+        if the number is a multiple of both 3 and 5 print 'FizzBuzz',
+        if the number is not a multiple of either 3 or 5, print the number
+    This is a famous coding interview question inspired by this blog post:
+    https://imranontech.com/2007/01/24/using-fizzbuzz-to-find-developers-who-grok-coding/
+    NOTE:
+    Since this function has many outputs, you do not use the `return` statement to output your results.
+    Instead, you should use the `print` function directly inside of a for loop.
+        >>> fizz_buzz(50)
+        1
+        2
+        Fizz
+        4
+        Buzz
+        Fizz
+        7
+        8
+        Fizz
+        Buzz
+        11
+        Fizz
+        13
+        14
+        FizzBuzz
+        16
+        17
+        Fizz
+        19
+        Buzz
+        Fizz
+        22
+        23
+        Fizz
+        Buzz
+        26
+        Fizz
+        28
+        29
+        FizzBuzz
+        31
+        32
+        Fizz
+        34
+        Buzz
+        Fizz
+        37
+        38
+        Fizz
+        Buzz
+        41
+        Fizz
+        43
+        44
+        FizzBuzz
+        46
+        47
+        Fizz
+        49
+        Buzz
+    '''
+    
 # DO NOT MODIFY BELOW THIS LINE
 import doctest
 doctest.testmod(verbose=True)
