a9a06e26775a28df0f66c3866b79506f51dd90b6
Mike Izbicki <mike@izbicki.me>
2019-11-21 09:23:26 -0800

    lecture notes

--- lecture_notes/flask_app/app/routes.py
+++ lecture_notes/flask_app/app/routes.py
@@ -1,24 +1,44 @@
 from app import app
 
-from flask import request
+from flask import request, render_template, send_from_directory
 
 # decorator
-@app.route('/hello')
+@app.route('/')
 def hello():
-    return 'hello world'
+    return render_template(
+            'index.html',
+            #user='mike izbicki'
+            )
+
+@app.route('/login')
+def login():
+    return render_template(
+            'login.html'
+            )
+
+@app.route('/static/<path:path>')
+def style(path):
+    return send_from_directory('static',path)
 
 @app.route('/goodbye')
 def goodbye():
     return 'goodbye world'
 
-# /url?param=https://google.com
-@app.route('/url')
-def url():
+# /https://google.com
+@app.route('/https://<path:url>')
+def url(url):
     import requests
-    param=request.args.get('param','')
-    r=requests.get(param)
+    #param=request.args.get('param','')
+    #return url
+    r=requests.get('https://'+url)
     return r.text
 
 @app.errorhandler(404)
 def not_found(e):
     return '404: NOT FOUND.'
+
+# 200 - everything worked
+# 404 - file not found
+# 500 - internal server error
+
+
