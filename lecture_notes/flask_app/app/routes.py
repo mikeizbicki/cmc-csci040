4e50f1e35f43f5cc05f42d3846efb7644c4892ff
Mike Izbicki <mike@izbicki.me>
2019-11-26 09:26:05 -0800

    lecture notes

--- lecture_notes/flask_app/app/routes.py
+++ lecture_notes/flask_app/app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 
-from flask import request, render_template, send_from_directory
+from flask import request, render_template, send_from_directory, make_response
 
 # decorator
 @app.route('/')
@@ -10,11 +10,47 @@ def hello():
             #user='mike izbicki'
             )
 
-@app.route('/login')
+# http://localhost:5000/login?username=Test&password=Test
+# http://localhost:5000/login
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template(
-            'login.html'
-            )
+    # keys = usernames, values = passwords
+    login_credentials={
+        'Trump':'12345',
+        'Sanders':'password'
+    }
+    if request.cookies.get('username') is not None:
+        return 'You are logged in as: '+request.cookies.get('username')
+
+    # request.args.get = get method
+    # request.form.get = post method
+    username=request.form.get('user')
+    password=request.form.get('password')
+    if username in login_credentials and \
+        login_credentials[username]== password:
+        #return render_template(
+        #    'login.html',
+        #    successful=True
+        #    )
+        res = make_response(render_template(
+                'login.html',
+                successful=True
+                ))
+        res.set_cookie('username',username)
+        return res
+    else:
+        # access directly
+        if username is None and password is None:
+            return render_template(
+                'login.html'
+                )
+
+        # bad user/pass
+        else:
+            return render_template(
+                'login.html',
+                bad_userpass=True
+                )
 
 @app.route('/static/<path:path>')
 def style(path):
