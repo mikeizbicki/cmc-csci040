1088fa19069242dbcbfffa4991c693711b8c9c05
Mike Izbicki <mike@izbicki.me>
2019-12-05 00:13:08 -0800

    lab.py

--- lab_14/lab.py
+++ lab_14/lab.py
@@ -1,9 +1,7 @@
 '''
 In this lab, you will create a sqlite database that will serve as the database for your twitter clone.
 
-To complete this lab, implement the following steps and upload the resulting lab.py file to sakai.
-
-Steps:
+To complete this lab, implement the following steps in this lab.py file:
 
     1. Import sqlite and create a database called 'twitter_clone.db'
 
@@ -15,10 +13,22 @@ Steps:
 
     3. Insert 3 rows into your users table with whatever usernames and passwords you choose 
 
-    4. Read the password from the database for a specified user and print it to the screen
+    4. For a particular user X that you just added:
 
-    5. Insert 3 rows into the messages table; make sure that the datetime for each message is set to the current time when your computer inserts the rows
+        a. Select the user's password from the database and print it to the screen
 
-    6. Read the messages from the table and print them to the screen
+        b. Update the password for the user
+
+        c. Repeat step 4.a
+
+    5. Insert 3 rows into the messages table, one for each user; make sure that the datetime for each message is set to the current time when your computer inserts the rows
+
+    6. Read all the messages from the table and print them to the screen, including the sender and the timestamp
+
+    7. Delete the message from one of the users
+
+    8. Repeat step 6
+
+Upload the lab.py file and the output of your program to sakai.
 
 '''
