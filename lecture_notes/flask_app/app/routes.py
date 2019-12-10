b4a94741327aa3984d44c2e689cabbe678ffa4e4
Mike Izbicki <mike@izbicki.me>
2019-12-10 09:26:25 -0800

    lecture notes

--- lecture_notes/flask_app/app/routes.py
+++ lecture_notes/flask_app/app/routes.py
@@ -1,14 +1,61 @@
 from app import app
 
 from flask import request, render_template, send_from_directory, make_response
+import datetime
+import sqlite3
 
 # decorator
 @app.route('/')
 def hello():
+    # STEP 1: get the info from sqlite
+    connection = sqlite3.connect(
+        'twitter_clone.db'
+        )
+    # twitter_clone.db.journal
+    # twitter_clone.db.wal
+    cursor = connection.cursor()
+    sql = '''
+        SELECT * FROM messages;
+    '''
+    res = cursor.execute(sql)
+    rows = res.fetchall()
+    messages = []
+    for row in rows:
+        message = {}
+        message['sender'] = row[1] # sender
+        message['message'] = row[2] # message
+        message['timestamp'] = row[3] # timestamp
+        messages.append(message)
+
+    messages_static = [
+        { 'sender' : 'mike',
+          'message' : 'Hello world!',
+          'timestamp' : datetime.datetime.now()
+        },
+        { 'sender' : 'definitely_not_mike',
+          'message' : 'Goodbye world!',
+          'timestamp' : datetime.datetime.now()
+        },
+    ]
+    # message['sender']
+
+    # STEP 2: render that information to HTML
+    username = request.cookies.get('username')
     return render_template(
             'index.html',
+            messages=messages,
             #user='mike izbicki'
+            user=username
             )
+
+@app.route('/logout')
+def logout():
+    res = make_response(render_template(
+            'index.html'
+            ))
+    res.set_cookie('username','',expires=0)
+    return res
+
 
 # http://localhost:5000/login?username=Test&password=Test
 # http://localhost:5000/login
