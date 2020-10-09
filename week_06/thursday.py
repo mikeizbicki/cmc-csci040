759a69a51b569fa2371c14355f374e3a6568d029
Mike Izbicki <mike@izbicki.me>
2020-10-08 17:20:14 -0700

    update lab notes

--- week_06/thursday.py
+++ week_06/thursday.py
@@ -1,10 +1,29 @@
 import praw
 
+# login to reddit with your bot's credentials;
+# recall that this requires creating a praw.ini file
 reddit = praw.Reddit('bot')
 
+# connect to the reddit submission
 submission = reddit.submission(url='https://www.reddit.com/r/csci040/comments/dw53wt/political_discussion_thread/')
 
+# click the "view more comments" buttons in the reddit submission in order to download all comments;
+# when debugging, I recommend setting this to a small value so that it runs faster;
+# it takes reddit 1-3 seconds per click, and so it takes about 10 minutes to click all the buttons;
 submission.comments.replace_more(limit=1)
 
+# loop through all the top level comments
+print('='*40)
+print('top level comments')
+print('='*40)
 for comment in submission.comments:
-    print('comment.author=',comment.author)
+    pass
+    # FIXME: calculate comment statistics here
+
+# loop through all the comments
+print('='*40)
+print('all comments')
+print('='*40)
+for comment in submission.comments.list():
+    # FIXME: calculate comment statistics here
+    pass
