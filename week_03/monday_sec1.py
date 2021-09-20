424a334fa8ebea28a2f11c421781d3e3023a60b0
Mike Izbicki <mike@izbicki.me>
2021-09-20 11:01:03 -0700

    week_03

---
+++ week_03/monday_sec1.py
@@ -0,0 +1,35 @@
+def largest(xs):
+    '''
+    Return the largest element in the input list.
+    If the list has less no elements, return None.
+
+    >>> largest([1,2,3])
+    3
+    >>> largest([99,-56,80,100,90])
+    100
+    >>> largest(list(range(0,100)))
+    99
+    >>> largest([10])
+    10
+    >>> largest([])
+    '''
+
+
+def filter_odd(xs):
+    '''
+    Return a list with all the odd elements removed.
+
+    HINT:
+    Use the accumulator pattern with a for loop.
+
+    >>> filter_even([1,3,5])
+    []
+    >>> filter_even([2,4,6])
+    [2, 4, 6]
+    >>> filter_even([4,5,6,7])
+    [4, 6]
+    >>> filter_even([20,13,4,16,8,19,10])
+    [20, 4, 16, 8, 10]
+    '''
+
+
