aa8ac033b2dff84ef6eb26a728cd1a394593b074
Mike Izbicki <mike@izbicki.me>
2021-09-27 15:59:13 -0700

    week_04

--- week_04/monday_sec1.py
+++ week_04/monday_sec1.py
@@ -9,3 +9,10 @@ def delete_HTML(text):
     >>> delete_HTML('This is <strong>italic</strong> and this is <ins>strikethrough</ins>!')
     'This is italic and this is strikethrough!'
     '''
+
+    # accumulator 
+    new_text = ''
+    for i in range(len(text)):
+        # do something with text[i]
+        new_text += text[i]
+    return new_text
