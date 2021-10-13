002c014bdf9e842a5ce24539da0291f9f8001b9f
Mike Izbicki <mike@izbicki.me>
2021-10-13 10:20:40 -0700

    week_06

---
+++ week_06/wednesday_sec1_part1.py
@@ -0,0 +1,17 @@
+tweets = [
+    {   "source": "Twitter Web Client", 
+        "id_str": "6312794445", 
+        "text": "Trump International Tower in Chicago ranked 6th tallest building in world by Council on Tall Buildings & Urban Habitat http://bit.ly/sqvQq", 
+        "created_at": "Thu Dec 03 19:39:09 +0000 2009", 
+        "retweet_count": 33,
+        "favorite_count": 6
+    },
+    {   "source": "Twitter Web Client", 
+        "id_str": "6971079756",
+        "text": "From DONALD Trump: Wishing everyone a wonderful holiday & a happy, healthy, prosperous New Year. Let's think like champions in 2010!", 
+        "created_at": "Wed Dec 23 17:38:18 +0000 2009",
+        "retweet_count": 28,
+        "favorite_count": 12
+        
+    }
+]
