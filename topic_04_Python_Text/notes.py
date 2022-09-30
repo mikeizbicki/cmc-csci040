4b8e4b209e6af6691f0ade1653e9664684d195d6
Mike Izbicki <mike@izbicki.me>
2022-09-30 10:57:43 -0700

    notes

--- topic_04_Python_Text/notes.py
+++ topic_04_Python_Text/notes.py
@@ -9,3 +9,31 @@ def delete_HTML(text):
     >>> delete_HTML('This is <strong>italic</strong> and this is <ins>strikethrough</ins>!')
     'This is italic and this is strikethrough!'
     '''
+
+    '''
+    # this works for the test cases, but it's not very general
+    # not the best solution
+    text = text.replace('<b>', '')
+    text = text.replace('</b>', '')
+    text = text.replace('<i>', '')
+    text = text.replace('</i>', '')
+    return text
+    '''
+
+    accumulator = ''
+    inside_html_tag = False
+    # will be true after a < and False after a >
+    # will be true when we're in the html tag; and false when outside
+    for c in text:
+        if c == '<':
+            inside_html_tag = True
+
+        # if we are inside of an HTML tag:
+        # if c != '>' and inside_html_tag == False:
+        if inside_html_tag == False:
+            accumulator += c
+        
+        if c == '>':
+            inside_html_tag = False
+        # do something with c
+    return accumulator
