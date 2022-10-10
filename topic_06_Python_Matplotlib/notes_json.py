bb84c83de8250f165ed5f3e6b1b171f8576c8ac0
Mike Izbicki <mike@izbicki.me>
2022-10-10 09:15:06 -0700

    notes

--- topic_06_Python_Matplotlib/notes_json.py
+++ topic_06_Python_Matplotlib/notes_json.py
@@ -15,33 +15,3 @@ tweets_str = '''[
         
     }
 ]'''
-
-# why do we care?
-# we know how to load strings from files
-# special type of file called JSON files which contain python code that can be loaded 
-# with the json.loads function
-
-"""
-files = ['data/condensed_2009.json', 'data/condensed_2010.json', 'data/condensed_2011.json' ]
-text = ''
-for file in files:
-    with open(file, encoding='ascii') as f:
-        text = f.read()
-"""
-
-with open('data/condensed_2016.json', encoding='ascii') as f:
-    text = f.read()
-
-import json
-tweets = json.loads(text)
-
-num_trumps = 0
-for tweet in tweets:
-    #if 'Trump' in tweet['text']:
-    #if 'Trump' in tweet['text'] or 'trump' in tweet['text'] or 'TRUMP' in tweet['text']:
-    #if 'trump' in tweet['text'].lower():
-    if tweet['text'].find('trump') != -1:
-        num_trumps += 1
-print('num_trumps=', num_trumps)
-
-
