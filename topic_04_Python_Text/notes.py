76c6023cf94da8cec65717c843235fff069d370f
Mike Izbicki <mike@izbicki.me>
2025-02-20 13:13:15 -0800

    topic04

---
+++ topic_04_Python_Text/notes.py
@@ -0,0 +1,11 @@
+def delete_HTML(text):
+    '''
+    This function removes all HTML tags from the input text.
+
+    >>> delete_HTML('This is <b>bold</b>!')
+    'This is bold!'
+    >>> delete_HTML('This is <i>italic</i>!')
+    'This is italic!'
+    >>> delete_HTML('This is <strong>italic</strong> and this is <ins>strikethrough</ins>!')
+    'This is italic and this is strikethrough!'
+    '''
