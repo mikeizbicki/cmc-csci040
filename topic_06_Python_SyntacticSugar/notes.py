8dc8c718ed2cac5eec3425f73b778013b478c4a6
Mike Izbicki <mike@izbicki.me>
2026-03-04 09:39:40 -0800

    syntactic sugar

--- topic_06_Python_SyntacticSugar/notes.py
+++ topic_06_Python_SyntacticSugar/notes.py
@@ -15,21 +15,16 @@
 # ```
 
 
-# Problem 4
+# Problem 21
 '''
 names = ['alice', 'bob', 'charlie', 'dave', 'eve']
 greetings = ['hello ' + name for name in names]
-#greetings = []
-#for name in names:
-#    greetings.append('hello ' + name)
-#print('greetings=', greetings)
 greeting = greetings[2]
 print('greeting=', greeting)
 '''
 
-# Problem 5
+# Problem 22
 '''
-# range(10) = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 xs = [ x*x for x in range(10) ]
 num = xs[5]
 print('num=', num)
@@ -52,26 +47,19 @@ print('num=', num)
 # ```
 
 
-# Problem 6
+# Problem 25
 '''
-#xs = [ x*x for x in range(10) if x%2 ]
-xs = []
-for x in range(10):
-    #if x%2:
-    if x%2 != 0:
-        xs.append(x*x)
-#print('xs=', xs)
+xs = [ x*x for x in range(10) if x%2 ]
 num = xs[3]
 print('num=', num)
 '''
 
-# Problem 9
+# Problem 26
+'''
 sentence = 'This is an example sentence with a few words in it.'
-#sentence = ' is an    a in '
-# ['This', 'is', 'an', ..., 'it.']
 small_words = [ word.lower() for word in sentence.split() if len(word) <= 2]
 print('len(small_words)=', len(small_words))
-
+'''
 
 
 ########################################
@@ -80,54 +68,27 @@ print('len(small_words)=', len(small_words))
 
 # If you have nested comprehensions,
 # repeat the desugaring steps above from outside to inside.
+
+# Problem 35
+'''
+xss = [[i for i in range(x)] for x in [2, 3, 4] if x%2 == 0]
+x = xss[-1][-2]
+print('x=', x)
+'''
+
+########################################
+# List Comprehensions (4)
+########################################
+
 # If you have a comprehension with multiple `for` statements,
 # repeat the desugaring steps above from left to right.
 
-# Problem 15
-'''
-#xss = [[i for i in range(x)] for x in [2, 3, 4] if x%2 == 0]
-xss = []
-for x in [2, 3, 4]:
-    if x%2 == 0:
-        xs = [i for i in range(x)]
-        # x = 2: xs = [0, 1]
-        # x = 4: xs = [0, 1, 2, 3]
-        #xs = []
-        #for i in range(x):
-        #    xs.append(i)
-        xss.append(xs)
-# xss = [[0, 1], [0, 1,2, 3]]
-print('15: xss=', xss)
-#x = xss[-1][-2]
-#print('x=', x)
-'''
-
-# Problem 18
+# Problem 40
 '''
 xss = [i for x in [2, 3, 4] if x%2 == 0 for i in range(x)]
-xss = []
-for x in [2, 3, 4]:
-    if x%2 == 0:
-        for i in range(x):
-            xss.append(i)
-print('18 xss=', xss)
-#x = xss[-2]
-#print('x=', x)
+x = xss[-1][-2]
+print('x=', x)
 '''
-
-# Problem 19:
-#xss = [ i for x in [4, 5, 6]  if x%2 == 1 for i in range(x) if i%3== 1 ]
-xss = []
-for x in [4, 5, 6]:
-    if x%2 == 1:
-        # x = 5
-        for i in range(x):
-            if i%3== 1:
-                xss.append(i)
-print('xss=', xss) # [1, 4]
-#x = xss[-1]
-#print('x=', x)
-
 
 ########################################
 # List Comprehensions (4)
@@ -170,7 +131,7 @@ tweets = [
     }]
 
 # Problem 21
+'''
 trump_tweets = [tweet for tweet in tweets if 'trump' in tweet['text'].lower()]
-#import pprint
-#pprint.pprint(trump_tweets)
 print('len(trump_tweets)=',len(trump_tweets))
+'''
